7b70840359f066267e3c78cbdf19809944f20a83
Deiroo <renatoballon.rb@gmail.com>
2019-09-16 00:28:29 -0500

    members of team query

--- src/main/java/com/soccermatch/SoccerMatch/repository/IMembershipsRepository.java
+++ src/main/java/com/soccermatch/SoccerMatch/repository/IMembershipsRepository.java
@@ -1,9 +1,17 @@
 package com.soccermatch.SoccerMatch.repository;
 
 import com.soccermatch.SoccerMatch.entity.Memberships;
+import com.soccermatch.SoccerMatch.entity.People;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMembershipsRepository extends JpaRepository<Memberships,Integer> {
+	
+	@Query("select p from People p join Membership m on m.person_id = p.id where m.team_id = ?1")
+	List<People>fetchTeamMembers(Integer id);
 }
