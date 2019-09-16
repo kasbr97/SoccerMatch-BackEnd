971f65572e144b82f51bc15b532bcfaf9422c846
Deiroo <renatoballon.rb@gmail.com>
2019-09-16 00:39:05 -0500

    commit test

--- src/main/java/com/soccermatch/SoccerMatch/repository/IFieldsRepository.java
+++ src/main/java/com/soccermatch/SoccerMatch/repository/IFieldsRepository.java
@@ -1,9 +1,16 @@
 package com.soccermatch.SoccerMatch.repository;
 
 import com.soccermatch.SoccerMatch.entity.Fields;
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IFieldsRepository extends JpaRepository<Fields,Integer> {
+	@Query("select f from Field f join Place p on f.place_id = p.id where p.id = ?1")
+	List<Fields>fetchTeamMembers(Integer id);
 }
